a7a864267e5ebf390b840e2a06e3e09e226e051d
muhnizar <mooh.nizar@yahoo.com>
2016-01-26 19:35:55 +0700

    +add alert dialog fragment

--- app/src/main/java/com/example/moohn/dialogframenttutorial/AlertDialogFragment.java
+++ app/src/main/java/com/example/moohn/dialogframenttutorial/AlertDialogFragment.java
@@ -3,18 +3,35 @@ package com.example.moohn.dialogframenttutorial;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.widget.Toast;
 
-/**
- * Created by moohn on 1/20/2016.
- */
 public class AlertDialogFragment extends DialogFragment{
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         AlertDialog.Builder alertDialog = new AlertDialog.Builder(getActivity());
-        alertDialog.setTitle("Alert!");
-        alertDialog.setMessage("This is a notication dialog");
+        alertDialog.setIcon(android.R.drawable.stat_notify_error);
+        alertDialog.setTitle("Alert dialog fragment example");
+        alertDialog.setMessage("This is message");
+
+//        positive botton
+        alertDialog.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getActivity(),"Pressed OK", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        alertDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getActivity(),"Cancel", Toast.LENGTH_LONG).show();
+            }
+        });
+
+
         return alertDialog.create();
 
     }
