8e9d296e1f8ae479b8ecda77062a4a1c1dbce672
muhnizar <mooh.nizar@yahoo.com>
2016-01-29 05:49:37 +0700

    +fixing bugs for options mens

--- app/src/main/java/com/example/moohn/dialogframenttutorial/MainActivity.java
+++ app/src/main/java/com/example/moohn/dialogframenttutorial/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends Activity {
         return true;
     }
 
-
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // close existing dialog fragments
         FragmentManager manager = getFragmentManager();
